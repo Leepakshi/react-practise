83fe731836a8c1127b836a818c226522f46f3539
Leepakshi Dhawan <leepakshidhawan@Leepakshis-MacBook-Pro.local>
2025-07-21 14:13:07 +0530

    initial chnges

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 {
   /* <div id="parent">
   <div id="child1">
@@ -17,7 +20,7 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I am h2 tag of child 1"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "I am h1 tag of child 2"),
+    React.createElement("h1", { id: "heading2" }, "I am h1 tag of child 2"),
     React.createElement("h2", {}, "I am h2 tag of child 2"),
   ]),
 ]);
